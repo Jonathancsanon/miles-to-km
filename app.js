47ee4bb64f6147fd90a705eca4c6b71a05b1376e
JonathanSanon <jonathancsanon@gmail.com>
2024-01-24 14:39:15 -0500

    first draft

---
+++ app.js
@@ -0,0 +1,9 @@
+const mile = 80.78;
+const kilo = 1.60934;
+
+
+const result = mile * kilo;
+const message = "welcome to our converter app";
+
+const displayMessage = `the distance equal to : ${result}`;
+console.log(displayMessage)
